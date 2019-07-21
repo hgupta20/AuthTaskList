633d4e66a7f2a38d127bb22856d206f2fe84ff90
hgupta20 <harshdevprakashgupta@outlook.com>
2019-07-20 19:57:53 -0500

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Welcome</div>
+
+                <div class="panel-body">
+                    Hi there, welcome to the Basic Task List, please register if you are new.
+                    If you already are a user, jump right in.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
